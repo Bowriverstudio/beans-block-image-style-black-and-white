c694a971e3648b3c242dfdf7b7cc588aadf93e64
Maurice Tadros <maurice.tadros@gmail.com>
2020-01-29 10:31:29 -0700

    Added boiler plate code

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# beans-block-image-style-black-and-white
+This contains boiler plate code related to setting up a Gutenberg Block
+
+Step 1# Copy Block To new Plugin
+Step 2# Remove .git folder if exists
+Step 3# Find and Replace
+
+- Namespace:   Beans\Block\Image\Style\BW
+- Plugin Name: Gutenberg BoilerPlate
+- enqueue_prefix  -  'beans-block-image-style-bw'
+
+
+------------------------------
+
+```npm init```
+```npm start```
+
---
+++ plugin.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Beans\Block\Image\Style\BW
+ *
+ * @package      Beans\Block\Image\Style\BW
+ * @author       Maurice Tadros, Yaidel Ferralize, Disnel Rodriguez
+ * @license      GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Beans Block Image Black and White
+ * Plugin URI:        na
+ * Description:       Adds an optional style to Gutenberg Block Image, that forces it to be black and white / then color on hover.
+ * Version:           1.0.0
+ * Author:            Maurice Tadros, Yaidel Ferralize, Disnel Rodriguez
+ * Author URI:        https://bowriverstudio.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI: na
+ * Requires PHP:      7.1
+ * Requires WP:       5.3
+ */
+
+
+namespace Beans\Block\Image\Style\BW;
+
+if (!defined('ABSPATH')) {
+    exit('Hello, Hello, Hello, what\'s going on here then?');
+}
+
+/**
+ * Gets this plugin's absolute directory path.
+ *
+ * @since  1.0.0
+ * @ignore
+ * @access private
+ *
+ * @return string
+ */
+function _get_plugin_directory() {
+    return wp_normalize_path( trailingslashit(dirname( __FILE__ )));
+}
+
+/**
+ * Gets this plugin's URL.
+ *
+ * @since  1.0.0
+ * @ignore
+ * @access private
+ *
+ * @return string
+ */
+function _get_plugin_url() {
+    static $plugin_url;
+
+    if ( empty( $plugin_url ) ) {
+        $plugin_url = plugins_url( null, __FILE__ ) .'/';
+    }
+
+    return $plugin_url;
+}
+
+// Enqueue JS and CSS.
+ include __DIR__ . '/lib/register-scripts.php';
+
